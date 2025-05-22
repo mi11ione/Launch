493626d1b7d55b1ec2a1da9f54f3e5fb45dd570e
mi11ione <i@mi11ion.io>
2025-05-22 18:58:53 +0300

    Initial Commit

---
+++ TravelSwag567/TravelSwag567App.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSwag567App.swift
+//  TravelSwag567
+//
+//  Created by Roman Zhuzhgov on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelSwag567App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
